db38bfdb5b239a47cfe9aa5155eecc96b5fc08ac
Todd Kirby <ffmpeg.php@gmail.com>
2022-09-08 22:15:00 +0700

    Fix target name, source path and add platform

--- Package.swift
+++ Package.swift
@@ -4,25 +4,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "ios-twitter-text-counter",
+    name: "VSTwitterTextCounter",
+    platforms: [
+       .iOS(.v8)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "ios-twitter-text-counter",
-            targets: ["ios-twitter-text-counter"]),
+      	
+	.library(
+            name: "VSTwitterTextCounter",
+            targets: ["VSTwitterTextCounter"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+
+    dependencies: [],
+
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "ios-twitter-text-counter",
-            dependencies: []),
-        .testTarget(
+        
+	.target(name: "VSTwitterTextCounter", dependencies: [], path: "VSTwitterTextCounter/Classes")
+        
+	.testTarget(
             name: "ios-twitter-text-counterTests",
-            dependencies: ["ios-twitter-text-counter"]),
+            dependencies: ["VSTwitterTextCounter"]),
     ]
 )
